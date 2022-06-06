6d6c92dad871b3a40f3295015287845d402e54c2
omar <omarmc2@gmail.com>
2022-06-06 17:42:30 -0500

    adding complements of security to API

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,7 @@
 const { query } = require('express');
 const response = require('express');
+const bcryptjs = require('bcryptjs');
+const User = require('../models/user');
 
 const userGet = (req, res = response )=> {
     
@@ -23,15 +25,36 @@ const userPut = (req, res = response)=> {
     console.log( 'put API-Controller', id )
 }
 
-const userPost = (req, res = response)=> {
+const userPost = async (req, res = response)=> {
+
+    const {name, email, password, role} = req.body;
+
+    const user = new User( {name, email, password, role} );
+
+    //Verify if email exist in DB
+    const emailExist = await User.findOne({ email });
+    if( emailExist ){
+        return res.status(400).json({
+            msg: 'The email is already registered'
+        }) 
+    }
+
+    //Encript password
+    const salt = bcryptjs.genSaltSync();
+    user.password = bcryptjs.hashSync( password, salt );
+
+    //Save on DB
+
+
     
-    const body = req.body;
+
+    await user.save();
 
     res.json({
         msg: 'post API-Controller',
-        body
+        user
     })
-    console.log( 'post API-Controller', body )
+    console.log( 'post API-Controller', user)
 }
 
 const userDelete = (req, res)=> {
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,7 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 const { userGet, userPut, userPost, userDelete, userPatch } = require('../controllers/user');
+const { validateCamps } = require('../middlewares/validate-camps');
 
 const router = Router();
 
@@ -7,7 +9,14 @@ router.get('/', userGet);
 
 router.put('/:id', userPut);
 
-router.post('/', userPost);
+router.post('/', [
+   check( 'name', 'Name is necesary').not().isEmpty(),
+   check( 'password', 'Passowrd is necesary and must be have more than 6 characters').isLength({ min: 6 }),
+   check( 'email', 'Email is not valid').isEmail(),
+   check( 'role', 'Is not a valid role').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+   validateCamps
+   
+], userPost);
 
 router.patch('/', userPatch);
 
